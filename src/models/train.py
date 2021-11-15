3328eb8508f4afa5fe72d14533bf32ddb6cd05cf
Marco Enzo Squillacioti <Marco.E.Squillacioti@arriva.dk>
2021-11-15 09:47:16 +0100

    Simplified code

--- src/models/train.py
+++ src/models/train.py
@@ -5,40 +5,44 @@ from argparse import ArgumentParser
 from pathlib import Path
 import pandas as pd
 import pytorch_lightning as pl
-from .models import BC_Car_s1, BC_Car_s2
-from src.data.datamodules import CarDataModule_s1, CarDataModule_s2
+from src.models.models import BC_Car_s1, BC_Car_s2
+from src.data.datamodules import CarDataModule
 import warnings
 
 #TODO: Time_window vs time_delta tuning. Data download.
 
-def Car_train_s1(batch_size, lr, l2, num_workers, shuffle, ckpt=None):
-    dm = CarDataModule_s1(shuffle=shuffle, batch_size=batch_size, num_workers=num_workers)
-    area_centers = pd.read_csv((Path('.') / 'data' / 'processed' / 'areas.csv'), index_col=0)
-    cars = pd.unique(pd.read_csv(Path('.') / 'data' / 'interim' / 'rental.csv', usecols=[2]).iloc[:,0])
-    in_size = int(3+len(cars)+2*len(area_centers)) # Date, car models and locations, and demand
-    if ckpt is None:
-        model = BC_Car_s1(hidden_layers=(30*in_size, 20*in_size, 10*in_size), in_size=in_size, lr=lr, l2=l2)
+def get_ckpt_path(stage):
+    if args.load_version is not None:
+        ckpt_path = (Path('.') / 'models' / stage / 'lightning_logs' / str('version_' + str(args.load_version)) / 'checkpoints' / '*.ckpt')
+        return Path('.') / max(glob.glob(str(ckpt_path)), key=os.path.getmtime)
     else:
-        model = BC_Car_s1.load_from_checkpoint(ckpt)
-    return model, dm
+        return None
 
-def Car_train_s2(batch_size, lr, l2, num_workers, shuffle, ckpt=None):
-    dm = CarDataModule_s2(shuffle=shuffle, batch_size=batch_size, num_workers=num_workers)
+def car_train(s, batch_size, lr, l2, num_workers, shuffle, ckpt=None):
+    dm = CarDataModule(s=s, shuffle=shuffle, batch_size=batch_size, num_workers=num_workers)
     area_centers = pd.read_csv((Path('.') / 'data' / 'processed' / 'areas.csv'), index_col=0)
     cars = pd.unique(pd.read_csv(Path('.') / 'data' / 'interim' / 'rental.csv', usecols=[2]).iloc[:,0])
-    in_size = int(3+len(cars)+2*len(area_centers)) # Date, car models and locations, and demand
+    in_size = int(3+len(cars)+3*len(area_centers)) # Date, car model and location (current), amount of cars in all zones, and demand
     out_size = len(area_centers)
-    if ckpt is None:
-        model = BC_Car_s2(hidden_layers=(30*in_size, int(15*in_size+5*out_size), 10*out_size), in_out=(in_size, out_size), lr=lr, l2=l2)
-    else:
-        model = BC_Car_s2.load_from_checkpoint(ckpt)
+    if s == 'stage_1':
+        if ckpt is None:
+            model = BC_Car_s1(hidden_layers=(30*in_size, 20*in_size, 10*in_size), in_size=in_size, lr=lr, l2=l2)
+        else:
+            model = BC_Car_s1.load_from_checkpoint(ckpt)
+    elif s == 'stage_2':
+        if ckpt is None:
+            model = BC_Car_s2(hidden_layers=(30*in_size, int(15*in_size+5*out_size), 10*out_size), in_out=(in_size, out_size), lr=lr, l2=l2)
+        else:
+            model = BC_Car_s2.load_from_checkpoint(ckpt)
     return model, dm
 
-def run_stage(args, stage, model, dm):
+def run_stage(args, stage):
     ckpt = get_ckpt_path(stage)
     args_trainer = deepcopy(args)
     args_trainer.default_root_dir = args.default_root_dir + '/' + stage
     trainer = pl.Trainer.from_argparse_args(args_trainer)
+    model, dm = car_train(s=stage, batch_size=args.batch_size, lr=args.lr, l2=args.l2, 
+        num_workers=args.num_workers, shuffle=args.shuffle, ckpt=get_ckpt_path(stage))
     if args.fit:
         if args.auto_lr_find or (args.auto_scale_batch_size is not None):
             trainer.tune(model, dm)
@@ -52,23 +56,12 @@ def run_stage(args, stage, model, dm):
         else:
             trainer.test(model, dm, ckpt_path=ckpt)
 
-def get_ckpt_path(stage):
-    if args.load_version is not None:
-        ckpt_path = (Path('.') / 'models' / stage / 'lightning_logs' / str('version_' + str(args.load_version)) / 'checkpoints' / '*.ckpt')
-        return Path('.') / max(glob.glob(str(ckpt_path)), key=os.path.getmtime)
-    else:
-        return None
-
 def main(args):
     pl.seed_everything(seed=args.seed, workers=True)
     if args.s1:
-        model_s1, dm_s1 = Car_train_s1(batch_size=args.batch_size, lr=args.lr, l2=args.l2,
-         num_workers=args.num_workers, shuffle=args.shuffle, ckpt=get_ckpt_path("stage_1"))
-        run_stage(args, "stage_1", model_s1, dm_s1)
+        run_stage(args, "stage_1")
     if args.s2:
-        model_s2, dm_s2 = Car_train_s2(batch_size=args.batch_size, lr=args.lr, l2=args.l2,
-         num_workers=args.num_workers, shuffle=args.shuffle, ckpt=get_ckpt_path("stage_2"))
-        run_stage(args, "stage_2", model_s2, dm_s2)
+        run_stage(args, "stage_2")
 
     
 if __name__ == "__main__":
