9f0a530244cbe833240145253c25e3db5069f443
Marco Enzo Squillacioti <marcoenzo0501@gmail.com>
2022-03-03 11:27:09 +0100

    Added CQN train

--- src/models/train.py
+++ src/models/train.py
@@ -1,15 +1,17 @@
 import os
 from copy import deepcopy
 import glob
+from datetime import datetime
 from argparse import ArgumentParser
 from pathlib import Path
 from datetime import timedelta
 import pandas as pd
 import pytorch_lightning as pl
 from pytorch_lightning.callbacks.early_stopping import EarlyStopping
-from src.models.models import BC_Area_s1, BC_Area_s2, BCLSTM_Area_s1, DQN
+from src.models.models import BC_Area_s1, BC_Area_s2, BCLSTM_Area_s1, DQN, CQN
 from src.data.datamodules import AreaDataModule
 import warnings
+pd.options.mode.chained_assignment = None
 #warnings.filterwarnings('error')
 
 # TODO: Time_window vs time_delta tuning. Data download.
@@ -27,9 +29,11 @@ def setup_model_dm(args, s, ckpt=None):
     area_centers = pd.read_csv((Path('.') / 'data' / 'processed' / 'areas.csv'), index_col=0)
     cars = pd.unique(pd.read_csv(Path('.') / 'data' / 'interim' / 'rental.csv', usecols=[2]).iloc[:,0])
     if s == 'stage_1' or s == 'stage_2':
-        dm = AreaDataModule(s=s, lstm=args.lstm, shuffle=args.shuffle, batch_size=args.batch_size, num_workers=args.num_workers, n_zones=args.n_zones)
+        dm = AreaDataModule(s=s, lstm=args.lstm, shuffle=args.shuffle, batch_size=args.batch_size, num_workers=args.num_workers,
+         n_zones=args.n_zones
+         ,time_step=timedelta(minutes=60), time_end=datetime(2020,2,5,16,0,0))
         if s == 'stage_1':
-            in_size = int(3+len(cars)+3*len(area_centers)) # Date (and time), car models and location (current), amount of cars in all zones, and demand
+            in_size = int(7+24+len(cars)+3*len(area_centers)) # Date (and time), car models and location (current), amount of cars in all zones, and demand
             o = len(pd.read_csv(Path('.') / 'data' / 'processed' / 'actions.csv', usecols=[0]))
             if args.lstm:
                 if ckpt is None:
@@ -49,7 +53,7 @@ def setup_model_dm(args, s, ckpt=None):
                 else:
                     model = BC_Area_s1.load_from_checkpoint(ckpt)
         elif s == 'stage_2':
-            in_size = int(3+len(cars)+3*len(area_centers)) # Date, car models and location (current), amount of cars in all zones, and demand
+            in_size = int(7+24+len(cars)+3*len(area_centers)) # Date, car models and location (current), amount of cars in all zones, and demand
             out_size = len(area_centers)
             if ckpt is None:
                 model = BC_Area_s2(hidden_layers=(30*in_size, int(15*in_size+5*out_size), 10*out_size),
@@ -57,25 +61,42 @@ def setup_model_dm(args, s, ckpt=None):
             else:
                 model = BC_Area_s2.load_from_checkpoint(ckpt)
     elif s == 'dqn':
-        in_size = int(3+len(cars)+3*len(area_centers)) # Date, car models and location (current), amount of cars in all zones, and demand
+        in_size = int(7+24+len(cars)+3*len(area_centers)) # Date, car models and location (current), amount of cars in all zones, and demand
         out_size = len(area_centers)
+        dm = None
         model = DQN(
             in_out=(in_size, out_size),
             hidden_layers=(30*in_size, int(15*in_size+5*out_size), 10*out_size),
             lr=args.lr, l2=args.l2, gamma=args.gamma, sync_rate=args.sync_rate, buffer_capacity=args.buffer_capacity, 
             warm_up=args.warm_up, sample_size=args.sample_size, batch_size=args.batch_size, num_workers=args.num_workers,
-            eps_stop=args.eps_stop, eps_start=args.eps_start, eps_end=args.eps_end, double_dqn=args.ddqn)
-    return model, None
+            eps_stop=args.eps_stop, eps_start=args.eps_start, eps_end=args.eps_end, double_dqn=args.ddqn, normalise_reward=args.scale_r,
+            time_step=timedelta(minutes=args.time_step), 
+            #time_end=datetime(2020,2,5,16,0,0))
+            time_end=datetime(args.year_end, args.month_end, 1, 0, 0, 0))
+    elif s == 'cqn':
+        in_size = int(7+24+len(cars)+3*len(area_centers)) # Date, car models and location (current), amount of cars in all zones, and demand
+        out_size = len(area_centers)
+        dm = None
+        model = CQN(
+            in_out=(in_size, out_size),
+            hidden_layers=(30*in_size, int(15*in_size+5*out_size), 10*out_size), buffer_code=args.buffer_code,
+            lr=args.lr, l2=args.l2, gamma=args.gamma, sync_rate=args.sync_rate, buffer_capacity=args.buffer_capacity, 
+            warm_up=args.warm_up, sample_size=args.sample_size, batch_size=args.batch_size, num_workers=args.num_workers,
+            eps_stop=args.eps_stop, eps_start=args.eps_start, eps_end=args.eps_end, normalise_reward=args.scale_r)
+    return model, dm
 
 def run_stage(args, stage):
     ckpt = get_ckpt_path(args, stage)
     args_trainer = deepcopy(args)
     args_trainer.default_root_dir = args.default_root_dir + '/' + stage
-    if stage == 's1' or stage == 's2':
+    model, dm = setup_model_dm(args=args, s=stage, ckpt=ckpt)
+    if stage == 'stage_1' or stage == 'stage_2':
         trainer = pl.Trainer.from_argparse_args(args_trainer, callbacks=[EarlyStopping(monitor='measure', mode='max', patience=10)])
     elif stage == 'dqn':
-        trainer = pl.Trainer.from_argparse_args(args_trainer)
-    model, dm = setup_model_dm(args=args, s=stage, ckpt=ckpt)
+        trainer = pl.Trainer.from_argparse_args(args_trainer
+        , callbacks=[EarlyStopping(monitor='Total_reward', mode='max', patience=int(10*len(model.agent.sim.timepoints))/(args.sample_size/args.batch_size))])
+    elif stage == 'cqn':
+        trainer = pl.Trainer.from_argparse_args(args_trainer, callbacks=[EarlyStopping(monitor='Total_reward', mode='max', patience=3)])
     if args.fit:
         if args.auto_lr_find or (args.auto_scale_batch_size is not None):
             trainer.tune(model, datamodule=dm)
@@ -95,8 +116,10 @@ def main(args):
         run_stage(args, "stage_1")
     if args.s2:
         run_stage(args, "stage_2")
-    if args.dqn:
+    if args.dqn or args.ddqn:
         run_stage(args, "dqn")
+    if args.cqn:
+        run_stage(args, "cqn")
 
     
 if __name__ == "__main__":
@@ -118,6 +141,8 @@ if __name__ == "__main__":
     parser.add_argument('--lstm', action='store_true', help='Use LSTM model')
     parser.add_argument('--dqn', action='store_true', help='Use DQN model')
     parser.add_argument('--ddqn', action='store_true', help='Use Double DQN loss')
+    parser.add_argument('--cqn', action='store_true', help='Use Offline DQN model')
+    parser.add_argument('--scale_r', action='store_true', help='Normalise reward during training')
     parser.add_argument('--buffer_capacity', default=1000000, type=int, help='Buffer capacity for DQN')
     parser.add_argument('--sample_size', default=21936, type=int, help='Sample size from buffer')
     parser.add_argument('--gamma', default=0.99, type=float, help='Gamma for DQN')
@@ -127,6 +152,10 @@ if __name__ == "__main__":
     parser.add_argument('--eps_stop', default=10968, type=int, help='Stop reducing epsilon after --eps_stop steps')
     parser.add_argument('--sync_rate', default=1000, type=int, help='Sync Q and target network every --sync_rate steps')
     parser.add_argument('--n_zones', default=20, type=int, help='Number of zones when rebuilding dataset')
+    parser.add_argument('--year_end', default=2021, type=int, help='Time_end year')
+    parser.add_argument('--month_end', default=5, type=int, help='Time_end month')
+    parser.add_argument('--time_step', default=30, type=int, help='Time step in minutes')
+    parser.add_argument('--buffer_code', default=20206, type=str, help='Buffer to use in offline DQN')
     args = parser.parse_args()
     warnings.filterwarnings(action="ignore", category=pl.utilities.warnings.LightningDeprecationWarning)
     warnings.filterwarnings(action="ignore", category=FutureWarning)
